764ba2149d95fc314d47f850c866996d8840d8be
Wang Chen <jeffsan@gmx.com>
2022-01-07 13:08:56 -0500

    Update youtube.rb

--- _plugins/youtube.rb
+++ _plugins/youtube.rb
@@ -21,7 +21,7 @@ class YouTube < Liquid::Tag
 
   def render(context)
     # "<iframe width=\"#{@width}\" height=\"#{@height}\" src=\"http://www.youtube.com/embed/#{@id}\" frameborder=\"0\"allowfullscreen></iframe>"
-    "<div class=\"video-wrapper\"><iframe src=\"http://www.youtube.com/embed/#{@id}\" frameborder=\"0\" allowfullscreen></iframe></div>"
+    "<div class=\"video-wrapper\"><iframe src=\"https://www.youtube.com/embed/#{@id}\" frameborder=\"0\" allowfullscreen></iframe></div>"
   end
 
   Liquid::Template.register_tag "youtube", self
